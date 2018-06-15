57765beebfcb93130284d8917737565dce2c0795
Eric <ernest.shelyag@gmail.com>
2018-06-15 18:19:40 +0300

    init

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+$(window).on('load', function () {
+  $('#preloader').fadeOut();
+});
+
+$(function () {
+
+  header();
+
+  svg4everybody();
+
+});
+
+let ass = () => {
+  console.log('ass');
+};
+
+ass();
